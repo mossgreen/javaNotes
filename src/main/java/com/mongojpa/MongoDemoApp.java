59a0076e59a756554848d22aac34216b0b0049d5
Moss GU <gufeifei@outlook.com>
2019-11-26 18:03:19 +1300

    mongo repository

--- src/main/java/com/mongojpa/MongoDemoApp.java
+++ src/main/java/com/mongojpa/MongoDemoApp.java
@@ -1,23 +1,26 @@
 package com.mongojpa;
 
-import com.mongodb.bulk.UpdateRequest;
 import com.mongodb.client.result.UpdateResult;
 import com.mongojpa.converter.MoneyReadConverter;
 import com.mongojpa.model.Coffee;
+import com.mongojpa.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -28,11 +31,15 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 
 @SpringBootApplication
 @Slf4j
-public class MongoDemoApp implements ApplicationRunner {
+@EnableMongoRepositories
+public class MongoDemoApp implements CommandLineRunner {
 
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private CoffeeRepository coffeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApp.class, args);
@@ -44,13 +51,36 @@ public class MongoDemoApp implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(String... args) throws Exception {
+
         Coffee espresso = Coffee.builder()
                 .name("espresso")
                 .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
                 .createTime(LocalDateTime.now())
                 .updateTime(LocalDateTime.now())
                 .build();
+
+        Coffee latte = Coffee.builder()
+                .name("lattee")
+                .price(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        coffeeRepository.insert(Arrays.asList(espresso, latte));
+
+        coffeeRepository.findAll(Sort.by("name"))
+                .forEach(c -> log.info("saved Coffee {}", c));
+
+        Thread.sleep(3000);
+
+        latte.setPrice(Money.of(CurrencyUnit.of("CNY"), 35.0));
+        latte.setUpdateTime(LocalDateTime.now());
+        coffeeRepository.save(latte);
+        coffeeRepository.findByName("latte").forEach(c -> log.info("coffee {}", c));
+
+        coffeeRepository.deleteAll();
+        // use mogoTemplate
 
         Coffee saved = mongoTemplate.save(espresso);
         log.info("Coffee {}", saved);
---
+++ src/main/java/com/mongojpa/repository/CoffeeRepository.java
@@ -0,0 +1,11 @@
+package com.mongojpa.repository;
+
+import com.mongojpa.model.Coffee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends MongoRepository<Coffee, String> {
+    List<Coffee> findByName(String name);
+}
+
