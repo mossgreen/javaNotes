8263d9bf2854655dc263390fcdd9c8029496f1d7
Moss GU <gufeifei@outlook.com>
2019-12-17 21:40:19 +1300

    set up junit and lombok

--- src/main/java/com/coffeeDemo/model/OrderState.java
+++
@@ -1,5 +0,0 @@
-package com.coffeeDemo.model;
-
-public enum OrderState {
-    INIT, PAID, BREWING, BREWED, TAKEN,CANCELLED
-}
--- src/main/java/com/coffeeDemo/repository/CoffeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.coffeeDemo.repository;
-
-import com.coffeeDemo.model.Coffee;
-
-public interface CoffeeRepository extends BaseRepository<Coffee, Long> {
-}
